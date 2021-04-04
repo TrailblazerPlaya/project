a06f169191e17000e266e04b60b58e8d58237cc9
Animeboy <fornarapro@gmail.com>
2021-04-04 17:45:21 +0400

    practice-1.1

--- practice.js
+++ practice.js
@@ -1,5 +1,6 @@
 "use strickt";
 
+//practice-1
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 const personalMovieDB = {
     count: numberOfFilms,
@@ -13,5 +14,5 @@ const a = prompt("Один из последних просмотренных ф
       c = prompt("Один из последних просмотренных фильмов?", ""),
       d = prompt("На сколько оцените его?", "");
 personalMovieDB.movies[a] = b;      
-personalMovieDB.movies[c] = d;  
-console.log(personalMovieDB); 
+personalMovieDB.movies[c] = d;
+
