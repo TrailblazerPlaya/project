4bbc8c4a25c1f238ec5d85674e73cc06851cfe96
Animeboy <fornarapro@gmail.com>
2021-04-04 17:37:25 +0400

    practice-1

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
 "use strickt";
 
-//Циклы
-let num = 50;
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        //break;
-        continue;//пропускает тот шаг, который не нужен.
-    }
-    console.log(i);
-}
+//
