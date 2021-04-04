0dbf29d44f7e11098a91e6e021ddd02fa7810f9c
Animeboy <fornarapro@gmail.com>
2021-04-04 21:14:41 +0400

    function

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 "use strickt";
 
-//
+//Function declaration
+function showFirstMassage(text) {
+    console.log(text);
+}
+showFirstMassage("hello sooqa");
+
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+// console.log(calc(4, 10));
+// console.log(calc(33, 20));
+// console.log(calc(55, 12));
+
+function ret() {
+    let num = 50;
+
+
+    return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
+
+//Function expression
+const logger = function() {
+    console.log("hello");
+};
+logger();
+
+//Стрелочная функция
+const calc = (a, b) => a + b;
+console.log(calc(10, 5));
