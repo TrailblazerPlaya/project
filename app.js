72b40f7cf701c3e86ed6e9475e901024e458cf34
Animeboy <fornarapro@gmail.com>
2021-04-05 12:42:09 +0400

    metodi

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 "use strickt";
-
+//_______________________________
 //Function declaration
 function showFirstMassage(text) {
     console.log(text);
@@ -32,3 +32,30 @@ logger();
 //Стрелочная функция
 const calc = (a, b) => a + b;
 console.log(calc(10, 5));
+
+
+//Методы и свойства строк и чисел________
+const arr = [1, 2, 3, 5];
+console.log(arr.length);
+
+
+const str = "teSt";
+console.log(str.toLowerCase());
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));//с какого индекса начинается
+
+
+
+const logg = "Hello anime boy";
+// console.log(logg.slice(6, 11));
+// console.log(logg.slice(12, 15));
+console.log(logg.substr(5, 6));//вырезает из переменной
+
+const num = 12.6;
+console.log(Math.round(num));//округление до целого
+
+const lel = "12.2px";
+console.log(parseInt(lel));//из строки в число(не прямое назначение)
+console.log(parseFloat(lel));//из строки в число с дробными значениями
+
