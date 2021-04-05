a432bfcc55c755654a58d6e6a1835143d395dded
Animeboy <fornarapro@gmail.com>
2021-04-05 16:23:57 +0400

    object learn

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 "use strickt";
-//Callback
-function first () {
-    //Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
-first();
+//Object
 
-function second () {
-    console.log(2);
-}
-second();
-/////////////
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    color: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() { //Это метод объекта, который я создал
+        console.log("Test");
+    }
+};
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+options.makeTest();
 
-function done() {
-    console.log("Я прошел этот урок");
-}
 
-learnJS("JavaScript", done);
+const {border, bg} = options.color;//Деструктуризация объекта(вытащил)
+console.log(border);
+
+
+// console.log(Object.keys(options).length);
+
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     }else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
