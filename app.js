c6099ed58cfdcea91da90be54b0c462aa1176924
Animeboy <fornarapro@gmail.com>
2021-04-06 20:21:24 +0400

    practice-4

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const soldier = {
     health: 400,
     armor: 100,
     sayHello: function() {
-        console.log('Hello')
+        console.log('Hello');
     }
 };
 
-const john = {
-    health: 100
-};
+const john = Object.create(soldier);
+
+
+
+// const john = {
+//     health: 100
+// };
 
 /*john.__proto__ = soldier; Устаревшая запись*/
-Object.setPrototypeOf(john, soldier);
+// Object.setPrototypeOf(john, soldier);
 // console.log(john.armor);
 john.sayHello();
