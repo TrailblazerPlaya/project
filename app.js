07e202ac8a3cbb42500049716a5dff2b662706ad
Animeboy <fornarapro@gmail.com>
2021-04-07 16:39:06 +0400

    dinam.tipiz

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 "use strickt";
-//Прототипирование
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function() {
-        console.log('Hello');
-    }
-};
+/*Динамическая типизация*/
+//To string
+//1 старый
+console.log(typeof(String(null)));
 
-const john = Object.create(soldier);
+//2 конкатинация (способ офк говно)
+console.log(typeof(5 + ''));
+
+const num = 5;
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 25 + 'px';
 
 
+//To number
+//1
+console.log(typeof(Number('4')));
 
-// const john = {
-//     health: 100
-// };
+//2 Унарный плюс
+console.log(typeof((+'4')));
 
-/*john.__proto__ = soldier; Устаревшая запись*/
-// Object.setPrototypeOf(john, soldier);
-// console.log(john.armor);
-john.sayHello();
+//3
+console.log(typeof(parseInt("15px", 10)));
+
+
+//To boolean
+
+/*0, '', null, undefined, NaN; - это все FALSE*/
+
+//1
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+if (switcher) {
+    console.log('Working...');
+}
+
+//2
+console.log(typeof(Boolean('4')));
